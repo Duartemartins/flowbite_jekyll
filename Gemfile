6a9fff962c4a1dbd61b6637aac10300a7b998e38
duartemartins <duarteosrm@gmail.com>
2023-04-11 21:37:13 +0100

    jekyll + landing page layout

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ group :jekyll_plugins do
   gem 'jekyll-timeago'
   gem 'jekyll-postcss'
   gem 'jekyll-seo-tag'
+  gem 'jekyll-paginate-v2'
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 module.exports = {
     content: [
         "./node_modules/flowbite/**/*.js",
-        './_drafts/**/*.html',
-        './_includes/**/*.html',
-        './_layouts/**/*.html',
-        './_posts/*.md',
+        './_jekyll/_drafts/**/*.html',
+        './_jekyll/_includes/**/*.html',
+        './_jekyll/_layouts/**/*.html',
+        './_jekyll/_posts/*.md',
         './*.md',
         './*.html',
     ],
     theme: {
-        theme: {
-            extend: {},
+        extend: {
+            colors: {
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+            }
         },
     },
     plugins: [
